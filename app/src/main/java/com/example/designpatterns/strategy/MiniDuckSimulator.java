eade1b34fce9a0c671ffb29b2f19bf28137ff78d
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-27 22:09:23 +0300

    test

---
+++ app/src/main/java/com/example/designpatterns/strategy/MiniDuckSimulator.java
@@ -0,0 +1,9 @@
+package com.example.designpatterns.strategy;
+
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.perfomFly();
+    }
+}
