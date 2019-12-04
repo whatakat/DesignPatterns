23a4d088e9fe92c48a53dd86af20608b4b83184d
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-04 19:13:50 +0300

    SwingObserveExample go JFrame

--- app/src/main/java/com/example/designpatterns/classObserveAndObservable/SwingObserverExample.java
+++ app/src/main/java/com/example/designpatterns/classObserveAndObservable/SwingObserverExample.java
@@ -1,4 +1,16 @@
 package com.example.designpatterns.classObserveAndObservable;
 
 public class SwingObserverExample {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go;
+    }
+    public void go(){
+        frame = new JFrame();
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(new AngelListener());
+        button.sddActionListener(new DevilListener());
+    }
 }
