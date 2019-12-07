2db2d3b3018132d43cfbde436342dc7f639af27d
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-07 23:48:56 +0300

    test

--- app/src/main/java/com/example/designpatterns/decorator/StarbuzzCoffee.java
+++ app/src/main/java/com/example/designpatterns/decorator/StarbuzzCoffee.java
@@ -5,5 +5,11 @@ public class StarbuzzCoffee {
         Beverage beverage = new Espresso();
         System.out.println(beverage.getDescription()
         +" $"+beverage.cost());
+
+        Beverage beverage2 = new HouseBlend();
+        beverage2 = new Mocha(beverage2);
+        System.out.println(beverage2.getDescription()
+                +" $"+beverage2.cost());
+
     }
 }
