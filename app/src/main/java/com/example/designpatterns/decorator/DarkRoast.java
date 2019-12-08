69fbad727288ced780221cf245855ec04738cd5b
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-08 13:12:39 +0300

    add DarkRoast

---
+++ app/src/main/java/com/example/designpatterns/decorator/DarkRoast.java
@@ -0,0 +1,10 @@
+package com.example.designpatterns.decorator;
+
+public class DarkRoast extends Beverage {
+    public DarkRoast(){
+        description = "Dark Roast";
+    }
+    public double cost(){
+        return 0.99;
+    }
+}
