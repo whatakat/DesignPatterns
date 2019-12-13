cc79cfd3d00f381d7c2a48cb13e6241f71f70430
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-13 23:52:11 +0300

    add factory

---
+++ app/src/main/java/com/example/designpatterns/factory/Pizza.java
@@ -0,0 +1,31 @@
+package com.example.designpatterns.factory;
+
+import java.util.ArrayList;
+
+public abstract class Pizza {
+    String name;
+    String dough;
+    String sauce;
+    ArrayList<String> toppings = new ArrayList<String>();
+    void prepare(){
+        System.out.println("Preoaring "+ name);
+        System.out.println("Tossing dough...");
+        System.out.println("Adding sauce...");
+        System.out.println("Adding toppings...");
+        for (String topping: toppings){
+            System.out.println("   "+topping);
+        }
+    }
+    void bake(){
+        System.out.println("Bake for 25 minutes at 350");
+    }
+    void cut(){
+        System.out.println("Cutting the pizza into diagonal slices");
+        }
+        void  box(){
+            System.out.println("Place pizza in official PizzaStore box");
+    }
+    public String getName(){
+        return  name;
+    }
+}
