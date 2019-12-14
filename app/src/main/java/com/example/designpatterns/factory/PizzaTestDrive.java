5e413a5c0e8a74ef61b4995658f045749c060e6e
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-14 15:19:42 +0300

    test

--- app/src/main/java/com/example/designpatterns/factory/PizzaTestDrive.java
+++ app/src/main/java/com/example/designpatterns/factory/PizzaTestDrive.java
@@ -2,5 +2,8 @@ package com.example.designpatterns.factory;
 
 public class PizzaTestDrive {
     public static void main(String[] args) {
+        PizzaStore nyStore = new NYPizzaStore();
+        Pizza pizza = nyStore.orderPizza("cheese");
+        System.out.println("Ethan ordered a "+pizza.getName());
     }
 }
