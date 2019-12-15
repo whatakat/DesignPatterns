13e4b867e51e758a51eb0a83f6ac1490c7338bfb
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-15 15:58:36 +0300

    refactor class pizza

--- app/src/main/java/com/example/designpatterns/factory/Pizza.java
+++ app/src/main/java/com/example/designpatterns/factory/Pizza.java
@@ -4,18 +4,13 @@ import java.util.ArrayList;
 
 public abstract class Pizza {
     String name;
-    String dough;
-    String sauce;
-    ArrayList<String> toppings = new ArrayList<String>();
-    void prepare(){
-        System.out.println("Preparing "+ name);
-        System.out.println("Tossing dough...");
-        System.out.println("Adding sauce...");
-        System.out.println("Adding toppings...");
-        for (String topping: toppings){
-            System.out.println("   "+topping);
-        }
-    }
+    Dough dough;
+    Sauce sauce;
+    Veggies veggies[];
+    Cheese cheese;
+    Pepperoni pepperoni;
+    Clams clams;
+    abstract void prepare();
     void bake(){
         System.out.println("Bake for 25 minutes at 350");
     }
@@ -27,5 +22,11 @@ public abstract class Pizza {
     }
     public String getName(){
         return  name;
+    }
+    void setName(String name){
+        this.name = name;
+    }
+    public String toString(){
+        return "method toString";
     }
 }
