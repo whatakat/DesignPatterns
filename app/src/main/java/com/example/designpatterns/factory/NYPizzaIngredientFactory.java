a903659d10a26d116b8d7005f21faeefc71183c0
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-15 15:34:12 +0300

    change NYPizzaIngredientFactory

--- app/src/main/java/com/example/designpatterns/factory/NYPizzaIngredientFactory.java
+++ app/src/main/java/com/example/designpatterns/factory/NYPizzaIngredientFactory.java
@@ -2,6 +2,31 @@ package com.example.designpatterns.factory;
 
 public class NYPizzaIngredientFactory implements PizzaIngredientFactory {
     public Dough createDough() {
-        return new ThinCrustDough();
+        return null;
+    }
+
+    @Override
+    public Sauce createSauce() {
+        return null;
+    }
+
+    @Override
+    public Cheese createCheese() {
+        return null;
+    }
+
+    @Override
+    public Veggies[] createVeggies() {
+        return new Veggies[0];
+    }
+
+    @Override
+    public Pepperoni createPepperoni() {
+        return null;
+    }
+
+    @Override
+    public Clams createClam() {
+        return null;
     }
 }
