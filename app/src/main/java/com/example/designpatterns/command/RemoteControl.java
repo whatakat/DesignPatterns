224383161a5bea5d8b18ef3f5e1349ef13b63cc0
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-17 23:25:50 +0300

    add methods in class RemoteControl

--- app/src/main/java/com/example/designpatterns/command/RemoteControl.java
+++ app/src/main/java/com/example/designpatterns/command/RemoteControl.java
@@ -8,5 +8,20 @@ public class RemoteControl {
         onCommands = new Command[7];
         offComands = new Command[7];
 
+        Command noCommand = new NoCommand();
+        for (int i = 0; i <7 ; i++) {
+            onCommands[i] = noCommand;
+            offComands[i] = noCommand;
+        }
+    }
+    public void setCommand(int slot, Command onCommand, Command offCommand){
+        onCommands[slot] = onCommand;
+        offComands[slot] = offCommand;
+    }
+    public void onButtonWasPushed(int slot){
+        onCommands[slot].execute();
+    }
+    public void offButtonWasPushed(int slot){
+        offComands[slot].execute();
     }
 }
