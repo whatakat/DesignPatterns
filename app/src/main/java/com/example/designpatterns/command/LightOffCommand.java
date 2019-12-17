566f2fc30dd0fb591a69e08b1cf978a26d7f8595
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-17 23:35:22 +0300

    add light off

---
+++ app/src/main/java/com/example/designpatterns/command/LightOffCommand.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.command;
+
+public class LightOffCommand implements Command {
+    Light light;
+
+    public LightOffCommand(Light light){
+        this.light= light;
+    }
+    public void execute(){
+        light.off();
+    }
+}
+
