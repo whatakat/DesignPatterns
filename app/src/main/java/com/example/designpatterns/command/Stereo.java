5639a3e28ac173597dfa74f676ff25b35b24481a
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-17 23:40:41 +0300

    add stereo

---
+++ app/src/main/java/com/example/designpatterns/command/Stereo.java
@@ -0,0 +1,10 @@
+package com.example.designpatterns.command;
+
+public class Stereo {
+    public void on(){}
+    public void off(){}
+    public void setCd(){}
+    public void setDvd(){}
+    public void setRadio(){}
+    public void setVolume(){}
+}
