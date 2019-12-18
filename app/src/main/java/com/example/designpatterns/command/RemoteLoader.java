470650fec7b66c9ad7f04e21047c5d5f62958628
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-18 20:27:37 +0300

    remote test

--- app/src/main/java/com/example/designpatterns/command/RemoteLoader.java
+++ app/src/main/java/com/example/designpatterns/command/RemoteLoader.java
@@ -5,6 +5,18 @@ public class RemoteLoader {
         RemoteControl remoteControl = new RemoteControl();
         Light livingRoomLight = new Light("Living room");
         Light kitchenLight = new Light("Kitchen");
-        Stereo stereo = new Stereo();
+        Stereo stereo = new Stereo("Living Room");
+
+        LightOnCommand livingRoomLightOn =
+                new LightOnCommand(livingRoomLight);
+        LightOffCommand livingRoomLightOff =
+                new LightOffCommand(livingRoomLight);
+        LightOnCommand kitchenLightOn =
+                new LightOnCommand(kitchenLight);
+        LightOffCommand kitchenLightOff =
+                new LightOffCommand(kitchenLight);
+        StereoOnWithCDCommand stereoOnWithCD =
+                new StereoOnWithCDCommand(stereo);
+
     }
 }
