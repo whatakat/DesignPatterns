49560f8d8231596ab4ab794f31634b84c38e2951
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-18 20:48:23 +0300

    change Light, add comment

--- app/src/main/java/com/example/designpatterns/command/Light.java
+++ app/src/main/java/com/example/designpatterns/command/Light.java
@@ -1,11 +1,14 @@
 package com.example.designpatterns.command;
 
 public class Light {
+    String comment;
     public void on(){
-        System.out.println("Light is on");
+        System.out.println("Light is on "+comment);
     }
     public void off(){
         System.out.println("Light is off");
     }
-    public Light(String comment){}
+    public Light(String comment){
+        this.comment = comment;
+    }
 }
