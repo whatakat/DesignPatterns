8e156629daff5c6281437c800419c98d355866a7
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-18 20:44:23 +0300

    change Light

--- app/src/main/java/com/example/designpatterns/command/Light.java
+++ app/src/main/java/com/example/designpatterns/command/Light.java
@@ -4,6 +4,8 @@ public class Light {
     public void on(){
         System.out.println("Light is on");
     }
-    public void off(){}
+    public void off(){
+        System.out.println("Light is off");
+    }
     public Light(String comment){}
 }
