1885d1880c7340911de8529483baad64ed351ded
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-18 20:04:14 +0300

    add stereo on with CD command

---
+++ app/src/main/java/com/example/designpatterns/command/StereoOnWithCDCommand.java
@@ -0,0 +1,16 @@
+package com.example.designpatterns.command;
+
+public class StereoOnWithCDCommand implements Command {
+    Stereo stereo;
+
+    public StereoOnWithCDCommand(Stereo stereo){
+        this.stereo = stereo;
+    }
+
+    @Override
+    public void execute() {
+        stereo.on();
+        stereo.setCd();
+        stereo.setVolume();
+    }
+}
