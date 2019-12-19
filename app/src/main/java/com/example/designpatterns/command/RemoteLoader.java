a40a87591cdfa9ef802fbd256e5db5dbe1723e6a
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-19 15:47:08 +0300

    test remote loader

--- app/src/main/java/com/example/designpatterns/command/RemoteLoader.java
+++ app/src/main/java/com/example/designpatterns/command/RemoteLoader.java
@@ -6,6 +6,7 @@ public class RemoteLoader {
         Light livingRoomLight = new Light("Living room");
         Light kitchenLight = new Light("Kitchen");
         Stereo stereo = new Stereo("Living Room");
+        Stereo stereo1 = new Stereo("Bedroom");
 
         LightOnCommand livingRoomLightOn =
                 new LightOnCommand(livingRoomLight);
@@ -19,10 +20,15 @@ public class RemoteLoader {
                 new StereoOnWithCDCommand(stereo);
         StereoOffCommand stereoOff =
                 new StereoOffCommand(stereo);
+        StereoOnWithCDCommand stereoOnWithCdB =
+                new StereoOnWithCDCommand(stereo1);
+        StereoOffCommand stereoOffB =
+                new StereoOffCommand(stereo1);
 
         remoteControl.setCommand(0,livingRoomLightOn,livingRoomLightOff);
         remoteControl.setCommand(1, kitchenLightOn,kitchenLightOff);
         remoteControl.setCommand(2,stereoOnWithCD,stereoOff);
+        remoteControl.setCommand(3,stereoOnWithCdB,stereoOffB);
         System.out.println(remoteControl);
 
         remoteControl.onButtonWasPushed(0);
@@ -31,6 +37,8 @@ public class RemoteLoader {
         remoteControl.offButtonWasPushed(1);
         remoteControl.onButtonWasPushed(2);
         remoteControl.offButtonWasPushed(2);
+        remoteControl.onButtonWasPushed(3);
+        remoteControl.offButtonWasPushed(3);
 
     }
 }
