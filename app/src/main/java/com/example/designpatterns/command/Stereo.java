2c72092fd8d7ed7df3bd9a206724b176e1d83a50
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-19 15:24:25 +0300

    change Light, off

--- app/src/main/java/com/example/designpatterns/command/Stereo.java
+++ app/src/main/java/com/example/designpatterns/command/Stereo.java
@@ -1,9 +1,15 @@
 package com.example.designpatterns.command;
 
 public class Stereo {
-    public void on(){}
-    public void off(){}
-    public void setCd(){}
+    public void on(){
+        System.out.println("Stereo on");
+    }
+    public void off(){
+        System.out.println("Stereo off");
+    }
+    public void setCd(){
+        System.out.println("set CD");
+    }
     public void setDvd(){}
     public void setRadio(){}
     public void setVolume(int volume){}
