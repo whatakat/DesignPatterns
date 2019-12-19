c1d3da723cc733bf745e7bb9af6f43808ee5def4
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-19 15:51:05 +0300

    change stereo constructor

--- app/src/main/java/com/example/designpatterns/command/Stereo.java
+++ app/src/main/java/com/example/designpatterns/command/Stereo.java
@@ -3,19 +3,19 @@ package com.example.designpatterns.command;
 public class Stereo {
     String comment;
     public void on(){
-        System.out.println("Stereo on");
+        System.out.println("Stereo on "+comment);
     }
     public void off(){
-        System.out.println("Stereo off");
+        System.out.println("Stereo off "+comment);
     }
     public void setCd(){
-        System.out.println("set CD");
+        System.out.println("set CD "+comment);
     }
     public void setDvd(){
-        System.out.println("set DVD");
+        System.out.println("set DVD "+comment);
     }
     public void setRadio(){
-        System.out.println("set radio");
+        System.out.println("set radio "+comment);
     }
     public void setVolume(int volume){}
     public Stereo(String comment){
