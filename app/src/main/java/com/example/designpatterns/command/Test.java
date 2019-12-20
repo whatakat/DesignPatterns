c2c2f928e5608ce9c70d5e4e064b01ae8789eb17
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-20 19:11:41 +0300

    Test

--- app/src/main/java/com/example/designpatterns/command/Test.java
+++ app/src/main/java/com/example/designpatterns/command/Test.java
@@ -6,9 +6,29 @@ public class Test {
         Tv tv = new Tv("Living room");
         Stereo stereo = new Stereo("Living room");
 
+        RemoteControlWithUndo remoteControl = new RemoteControlWithUndo();
+
         LightOnCommand lightOn = new LightOnCommand(light);
         StereoOnWithCDCommand stereoOn = new StereoOnWithCDCommand(stereo);
         TVOnCommand tvOn = new TVOnCommand(tv);
+
+        LightOffCommand lightOff = new LightOffCommand(light);
+        StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+        TVOffCommand tvOff = new TVOffCommand(tv);
+
+        Command[] partyOn={lightOn,stereoOn,tvOn};
+        Command[] partyOff={lightOff,stereoOff,tvOff};
+
+        MacroCommand partyOnMacro = new MacroCommand(partyOn);
+        MacroCommand partyOffMacro = new MacroCommand(partyOff);
+
+        remoteControl.setCommand(0,partyOnMacro,partyOffMacro);
+
+        System.out.println(remoteControl);
+        System.out.println("---Pushing Macro On---");
+        remoteControl.onButtonWasPushed(0);
+        System.out.println("---Pushing Macro Off---");
+        remoteControl.offButtonWasPushed(0);
 
 
     }
