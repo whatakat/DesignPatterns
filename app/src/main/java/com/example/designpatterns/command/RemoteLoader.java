40bce10e962c34642cf7449c2200e722c2a1173a
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-20 17:06:51 +0300

    change class RemoteLoader for test

--- app/src/main/java/com/example/designpatterns/command/RemoteLoader.java
+++ app/src/main/java/com/example/designpatterns/command/RemoteLoader.java
@@ -3,44 +3,49 @@ package com.example.designpatterns.command;
 public class RemoteLoader {
     public static void main(String[] args) {
         RemoteControlWithUndo remoteControl = new RemoteControlWithUndo();
-        Light livingRoomLight = new Light("Living room");
-        Light kitchenLight = new Light("Kitchen");
-        Stereo stereo = new Stereo("Living Room");
-        Stereo stereo1 = new Stereo("Bedroom");
+        CeilingFan ceilingFan = new CeilingFan("Living Room");
 
-        LightOnCommand livingRoomLightOn =
-                new LightOnCommand(livingRoomLight);
-        LightOffCommand livingRoomLightOff =
-                new LightOffCommand(livingRoomLight);
-        LightOnCommand kitchenLightOn =
-                new LightOnCommand(kitchenLight);
-        LightOffCommand kitchenLightOff =
-                new LightOffCommand(kitchenLight);
-        StereoOnWithCDCommand stereoOnWithCD =
-                new StereoOnWithCDCommand(stereo);
-        StereoOffCommand stereoOff =
-                new StereoOffCommand(stereo);
-        StereoOnWithCDCommand stereoOnWithCdB =
-                new StereoOnWithCDCommand(stereo1);
-        StereoOffCommand stereoOffB =
-                new StereoOffCommand(stereo1);
+        CeilingFanHighCommand ceilingFanHigh =
+                new CeilingFanHighCommand(ceilingFan);
 
-        remoteControl.setCommand(0,livingRoomLightOn,livingRoomLightOff);
-        remoteControl.setCommand(1, kitchenLightOn,kitchenLightOff);
-        remoteControl.setCommand(2,stereoOnWithCD,stereoOff);
-        remoteControl.setCommand(3,stereoOnWithCdB,stereoOffB);
-        System.out.println(remoteControl);
-
-        remoteControl.onButtonWasPushed(0);
-        remoteControl.offButtonWasPushed(0);
-        System.out.println(remoteControl);
-        remoteControl.undoCommandWasPushed();
-        remoteControl.onButtonWasPushed(1);
-        remoteControl.offButtonWasPushed(1);
-        remoteControl.onButtonWasPushed(2);
-        remoteControl.offButtonWasPushed(2);
-        remoteControl.onButtonWasPushed(3);
-        remoteControl.offButtonWasPushed(3);
-
+//        Light livingRoomLight = new Light("Living room");
+//        Light kitchenLight = new Light("Kitchen");
+//        Stereo stereo = new Stereo("Living Room");
+//        Stereo stereo1 = new Stereo("Bedroom");
+//
+//        LightOnCommand livingRoomLightOn =
+//                new LightOnCommand(livingRoomLight);
+//        LightOffCommand livingRoomLightOff =
+//                new LightOffCommand(livingRoomLight);
+//        LightOnCommand kitchenLightOn =
+//                new LightOnCommand(kitchenLight);
+//        LightOffCommand kitchenLightOff =
+//                new LightOffCommand(kitchenLight);
+//        StereoOnWithCDCommand stereoOnWithCD =
+//                new StereoOnWithCDCommand(stereo);
+//        StereoOffCommand stereoOff =
+//                new StereoOffCommand(stereo);
+//        StereoOnWithCDCommand stereoOnWithCdB =
+//                new StereoOnWithCDCommand(stereo1);
+//        StereoOffCommand stereoOffB =
+//                new StereoOffCommand(stereo1);
+//
+//        remoteControl.setCommand(0,livingRoomLightOn,livingRoomLightOff);
+//        remoteControl.setCommand(1, kitchenLightOn,kitchenLightOff);
+//        remoteControl.setCommand(2,stereoOnWithCD,stereoOff);
+//        remoteControl.setCommand(3,stereoOnWithCdB,stereoOffB);
+//        System.out.println(remoteControl);
+//
+//        remoteControl.onButtonWasPushed(0);
+//        remoteControl.offButtonWasPushed(0);
+//        System.out.println(remoteControl);
+//        remoteControl.undoCommandWasPushed();
+//        remoteControl.onButtonWasPushed(1);
+//        remoteControl.offButtonWasPushed(1);
+//        remoteControl.onButtonWasPushed(2);
+//        remoteControl.offButtonWasPushed(2);
+//        remoteControl.onButtonWasPushed(3);
+//        remoteControl.offButtonWasPushed(3);
+//
     }
 }
