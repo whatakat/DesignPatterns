f57427739abd49e0666bd679d2f1e9610fccb16b
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-22 20:57:00 +0300

    change HomeTheaterFacade

--- app/src/main/java/com/example/designpatterns/facade/HomeTheaterFacade.java
+++ app/src/main/java/com/example/designpatterns/facade/HomeTheaterFacade.java
@@ -1,5 +1,22 @@
 package com.example.designpatterns.facade;
 
+import com.example.designpatterns.command.CeilingFan;
+import com.example.designpatterns.command.Light;
+import com.example.designpatterns.command.Stereo;
+import com.example.designpatterns.command.Tv;
+
 public class HomeTheaterFacade {
+    Light light;
+    Tv tv;
+    Stereo stereo;
+    CeilingFan ceilingFan;
+
+    public HomeTheaterFacade(Light light, Tv tv, Stereo stereo, CeilingFan ceilingFan){
+        this.ceilingFan = ceilingFan;
+        this.light = light;
+        this.stereo = stereo;
+        this.tv = tv;
+    }
+
 
 }
