32a50da38647f6753e0c5ada5a7e574eff7c3299
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-23 18:29:40 +0300

    change Coffee

--- app/src/main/java/com/example/designpatterns/templateMethod/Coffee.java
+++ app/src/main/java/com/example/designpatterns/templateMethod/Coffee.java
@@ -1,23 +1,13 @@
 package com.example.designpatterns.templateMethod;
 
-public class Coffee {
-    void prepareRecipe(){
-        boilWater();
-        brefCoffeeGrinds();
-        pourInCup();
-        addSugarAndMilk();
+public class Coffee extends CaffeineBeverage {
+    @Override
+    void brew() {
+        System.out.println("dripping Coffee through filter");
+    }
 
-    }
-    public void boilWater(){
-        System.out.println("Boiling water");
-    }
-    public void brefCoffeeGrinds(){
-        System.out.println("Dripping Coffee through filter");
-    }
-    public void pourInCup(){
-        System.out.println("Pouring into cup");
-    }
-    public void addSugarAndMilk(){
+    @Override
+    void addCondiments() {
         System.out.println("Adding Sugar and Milk");
     }
 }
