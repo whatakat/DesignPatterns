7cb1075db2476d691a7ca501cc9d54aeb7afc8e3
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-23 18:27:44 +0300

    change Tea

--- app/src/main/java/com/example/designpatterns/templateMethod/Tea.java
+++ app/src/main/java/com/example/designpatterns/templateMethod/Tea.java
@@ -1,23 +1,10 @@
 package com.example.designpatterns.templateMethod;
 
-public class Tea {
-    void prepareRecipe(){
-        boilWater();
-        steepTeaBag();
-        addLemon();
-        pourInCup();
-
-    }
-    public void boilWater(){
-        System.out.println("Boiling water");
-    }
-    public void steepTeaBag(){
+public class Tea extends CaffeineBeverage{
+    public void brew(){
         System.out.println("Steeping the tea");
     }
-    public void addLemon(){
+    public void addCondiments(){
         System.out.println("Adding Lemon");
-    }
-    public void pourInCup(){
-        System.out.println("Pouring into cup");
     }
 }
