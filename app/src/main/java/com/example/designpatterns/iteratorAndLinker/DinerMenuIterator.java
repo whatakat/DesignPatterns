b4714bc1376ac66b0eabec9c421ebe9232422bb5
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-26 14:30:56 +0300

    change DinerMenuIterator

--- app/src/main/java/com/example/designpatterns/iteratorAndLinker/DinerMenuIterator.java
+++ app/src/main/java/com/example/designpatterns/iteratorAndLinker/DinerMenuIterator.java
@@ -1,5 +1,5 @@
 package com.example.designpatterns.iteratorAndLinker;
-
+import java.util.Iterator;
 
 public class DinerMenuIterator implements Iterator {
     MenuItem[] items;
@@ -17,6 +17,20 @@ public class DinerMenuIterator implements Iterator {
             return false;
         }else {
             return true;
+        }
+    }
+
+    @Override
+    public void remove() {
+        if (position<=0){
+            throw new IllegalStateException
+                    ("You can't remove an item util you've done at least one next()");
+        }
+        if (items[position-1]!= null){
+            for (int i = position-1; i <(items.length-1) ; i++) {
+                items[i]=items[i+1];
+            }
+            items[items.length-1]=null;
         }
     }
 }
