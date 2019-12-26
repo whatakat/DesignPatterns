d6e94e9221095d88eede9ff68ddfe1b2d5d68245
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-26 21:44:23 +0300

    change MenuTestDrive

--- app/src/main/java/com/example/designpatterns/iteratorAndLinker/MenuTestDrive.java
+++ app/src/main/java/com/example/designpatterns/iteratorAndLinker/MenuTestDrive.java
@@ -2,11 +2,11 @@ package com.example.designpatterns.iteratorAndLinker;
 
 public class MenuTestDrive {
     public static void main(String[] args) {
-        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
-        DinerMenu dinerMenu = new DinerMenu();
-        CafeMenu cafeMenu = new CafeMenu();
-        Waitress waitress = new Waitress(pancakeHouseMenu,dinerMenu,cafeMenu);
-        waitress.printMenu();
+        //PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        //DinerMenu dinerMenu = new DinerMenu();
+        //CafeMenu cafeMenu = new CafeMenu();
+        //Waitress waitress = new Waitress();//need change
+        //waitress.printMenu();
         System.out.println("End menu");
 
     }
