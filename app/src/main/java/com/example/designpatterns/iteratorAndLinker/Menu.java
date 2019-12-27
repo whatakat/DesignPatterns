253a0f9af193a26676e5cfcbc59f5e7071b8234a
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-27 15:47:51 +0300

    change Menu

--- app/src/main/java/com/example/designpatterns/iteratorAndLinker/Menu.java
+++ app/src/main/java/com/example/designpatterns/iteratorAndLinker/Menu.java
@@ -1,7 +1,35 @@
 package com.example.designpatterns.iteratorAndLinker;
 
-import java.util.Iterator;
 
-public interface Menu {
-    public Iterator<MenuItem> createIterator();
+import java.util.ArrayList;
+
+public class Menu extends MenuComponent {
+    ArrayList<MenuComponent> menuComponents = new ArrayList<MenuComponent>();
+    String name;
+    String description;
+
+    public Menu(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+    public void add(MenuComponent menuComponent){
+        menuComponents.add(menuComponent);
+    }
+    public void remove(MenuComponent menuComponent){
+        menuComponents.remove(menuComponent);
+    }
+    public MenuComponent getChild(int i){
+        return menuComponents.get(i);
+    }
+    public String getName(){
+        return name;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void print(){
+        System.out.print("\n"+getName());
+        System.out.println(", "+getDescription());
+        System.out.println("-------------------");
+    }
 }
