b47f41b1cdcba7ae212d71b4023eb901997325a7
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-28 16:12:30 +0300

    add NullIterator

---
+++ app/src/main/java/com/example/designpatterns/iteratorAndLinker/Nulliterator.java
@@ -0,0 +1,15 @@
+package com.example.designpatterns.iteratorAndLinker;
+import java.util.Iterator;
+
+public class Nulliterator implements Iterator<MenuComponent> {
+    public Object next(){
+        return null;
+    }
+    public boolean hasNext(){
+        return false;
+    }
+    public void remove(){
+        throw new UnsupportedOperationException();
+    }
+
+}
