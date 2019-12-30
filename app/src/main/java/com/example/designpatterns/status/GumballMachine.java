e196ebc2fb93b48077930a69db8ce5803b30fce8
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-30 23:44:08 +0300

    add method turnCrank()

--- app/src/main/java/com/example/designpatterns/status/GumballMachine.java
+++ app/src/main/java/com/example/designpatterns/status/GumballMachine.java
@@ -40,4 +40,17 @@ public class GumballMachine {
             System.out.println("You can't eject, you haven't inserted a quarter yet");
         }
     }
+    public void turnCrank(){
+        if (state ==SOLD){
+            System.out.println("Turning twice doesn't get you another gumball!");
+        }else if (state==NO_QUARTER){
+            System.out.println("You turned but there's no quarter");
+        }else if (state==SOLD_OUT){
+            System.out.println("You turned, but there are no gumballs");
+        }else if (state==HAS_QUARTER){
+            System.out.println("You turned...");
+            state=SOLD;
+
+        }
+    }
 }
