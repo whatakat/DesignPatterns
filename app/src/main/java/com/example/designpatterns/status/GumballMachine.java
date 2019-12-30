89f267bd0558862b8455b3635e1bf04d9497eaac
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-30 23:32:32 +0300

    add method insertQuarter()

--- app/src/main/java/com/example/designpatterns/status/GumballMachine.java
+++ app/src/main/java/com/example/designpatterns/status/GumballMachine.java
@@ -15,4 +15,17 @@ public class GumballMachine {
             state = NO_QUARTER;
         }
     }
+
+    public void insertQuarter(){
+        if (state == HAS_QUARTER){
+            System.out.println("You can't insert another quarter");
+        }else if (state == NO_QUARTER){
+            state = HAS_QUARTER;
+            System.out.println("You inserted a quarter");
+        }else if (state==SOLD_OUT){
+            System.out.println("You can't insert a quarter, the machine is sold out");
+        }else if (state==SOLD){
+            System.out.println("Please wait, we're already giving you a gumball");
+        }
+    }
 }
