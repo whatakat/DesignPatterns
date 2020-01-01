ad88fe2428a5064aace7afa404750d2a37d7fed7
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-01 23:56:44 +0300

    add methods

--- app/src/main/java/com/example/designpatterns/status/SoldState.java
+++ app/src/main/java/com/example/designpatterns/status/SoldState.java
@@ -3,16 +3,18 @@ package com.example.designpatterns.status;
 public class SoldState implements State {
     @Override
     public void insertQuarter() {
-
+        System.out.println("Please wait, we're already giving you a gumball");
     }
 
     @Override
     public void ejectQuarter() {
+        System.out.println("Sorry, you already turned the crank");
 
     }
 
     @Override
     public void turnCrank() {
+        System.out.println("Turning twice doesn't get you another gumball!");
 
     }
 
