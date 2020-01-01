89af993da8c2899d6e8e63d9eead987a44cc490e
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-01 23:53:41 +0300

    add SoldState

---
+++ app/src/main/java/com/example/designpatterns/status/SoldState.java
@@ -0,0 +1,23 @@
+package com.example.designpatterns.status;
+
+public class SoldState implements State {
+    @Override
+    public void insertQuarter() {
+
+    }
+
+    @Override
+    public void ejectQuarter() {
+
+    }
+
+    @Override
+    public void turnCrank() {
+
+    }
+
+    @Override
+    public void dispense() {
+
+    }
+}
