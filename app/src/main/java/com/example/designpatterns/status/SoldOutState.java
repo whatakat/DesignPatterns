4ac6e905e592b2289e4899d37a5b37c0f984f5fa
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-02 00:12:15 +0300

    add class SoldOutState

---
+++ app/src/main/java/com/example/designpatterns/status/SoldOutState.java
@@ -0,0 +1,25 @@
+package com.example.designpatterns.status;
+
+public class SoldOutState implements State {
+    GumballMachine gumballMachine;
+
+    @Override
+    public void insertQuarter() {
+
+    }
+
+    @Override
+    public void ejectQuarter() {
+
+    }
+
+    @Override
+    public void turnCrank() {
+
+    }
+
+    @Override
+    public void dispense() {
+
+    }
+}
