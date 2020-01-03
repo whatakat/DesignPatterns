9a91a9cab6b85687374c1b05b3b50958b5847519
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-03 12:14:10 +0300

    refactor HasQuarterState

--- app/src/main/java/com/example/designpatterns/status/HasQuarterState.java
+++ app/src/main/java/com/example/designpatterns/status/HasQuarterState.java
@@ -1,6 +1,9 @@
 package com.example.designpatterns.status;
 
+import java.util.Random;
+
 public class HasQuarterState implements State {
+    Random randomWinner = new Random(System.currentTimeMillis());
     GumballMachine gumballMachine;
     public HasQuarterState(GumballMachine gumballMachine){
         this.gumballMachine = gumballMachine;
@@ -20,7 +23,12 @@ public class HasQuarterState implements State {
     @Override
     public void turnCrank() {
         System.out.println("You turned...");
-        gumballMachine.setState(gumballMachine.getSoldState());
+        int winner = randomWinner.nextInt(10);
+        if ((winner ==0)&& (gumballMachine.getCount()>1)){
+            gumballMachine.setState(gumballMachine.getWinnerState());
+        }else {
+            gumballMachine.setState(gumballMachine.getSoldState());
+        }
     }
 
     @Override
