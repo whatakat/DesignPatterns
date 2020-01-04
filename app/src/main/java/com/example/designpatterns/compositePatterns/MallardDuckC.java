a6a3874e0b1c6c3a3a4558183185822ba80df5b7
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-04 14:24:12 +0300

    add class MallardDuckC

---
+++ app/src/main/java/com/example/designpatterns/compositePatterns/MallardDuckC.java
@@ -0,0 +1,8 @@
+package com.example.designpatterns.compositePatterns;
+
+public class MallardDuckC implements Quackable {
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+}
