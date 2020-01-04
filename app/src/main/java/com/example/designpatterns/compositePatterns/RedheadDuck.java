7d64e981ff32cedbc23e2544d836754f54fae058
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-04 14:25:52 +0300

    add class Redhead

---
+++ app/src/main/java/com/example/designpatterns/compositePatterns/RedheadDuck.java
@@ -0,0 +1,8 @@
+package com.example.designpatterns.compositePatterns;
+
+public class RedheadDuck implements Quackable {
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+}
