825fd2b24d4647398010c6090954390d4311a797
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-04 14:52:10 +0300

    test

--- app/src/main/java/com/example/designpatterns/compositePatterns/DuckSimulator.java
+++ app/src/main/java/com/example/designpatterns/compositePatterns/DuckSimulator.java
@@ -10,12 +10,14 @@ public class DuckSimulator {
         Quackable redheadDuck = new RedheadDuck();
         Quackable duckCall = new DuckCall();
         Quackable rubberDuck = new RubberDuck();
+        Quackable gooseDuck = new GooseAdapter(new Goose());
 
         System.out.println("\nDuck Simulator");
         simulate(mallardDuck);
         simulate(redheadDuck);
         simulate(duckCall);
         simulate(rubberDuck);
+        simulate(gooseDuck);
     }
     void simulate(Quackable duck){
         duck.quack();
