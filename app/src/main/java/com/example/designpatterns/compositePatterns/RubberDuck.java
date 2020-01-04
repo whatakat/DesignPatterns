b09c01e863e8565c6ffcb9693e579ed14da89f68
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-04 14:29:51 +0300

    add class RubberDuck

---
+++ app/src/main/java/com/example/designpatterns/compositePatterns/RubberDuck.java
@@ -0,0 +1,8 @@
+package com.example.designpatterns.compositePatterns;
+
+public class RubberDuck implements Quackable {
+    @Override
+    public void quack() {
+        System.out.println("Squeak");
+    }
+}
