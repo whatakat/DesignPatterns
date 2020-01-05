72138d3a98c9ba18cf0b6d9ab7515509e960d823
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-05 13:40:29 +0300

    refactor RubberDuck

--- app/src/main/java/com/example/designpatterns/compositePatterns/RubberDuck.java
+++ app/src/main/java/com/example/designpatterns/compositePatterns/RubberDuck.java
@@ -1,8 +1,27 @@
 package com.example.designpatterns.compositePatterns;
 
+import com.example.designpatterns.compositePatterns.observer.Observable;
+import com.example.designpatterns.compositePatterns.observer.Observer;
+
 public class RubberDuck implements Quackable {
+    Observable observable;
+    public RubberDuck(){
+        observable = new Observable(this);
+    }
     @Override
     public void quack() {
         System.out.println("Squeak");
+        notifyObservers();
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observable.registerObserver(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observable.notifyObservers();
+
     }
 }
