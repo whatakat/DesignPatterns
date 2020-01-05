fa0aebcc225646e538569a7f4fa72c3bf11adc33
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-05 13:45:46 +0300

    refactor DuckCall

--- app/src/main/java/com/example/designpatterns/compositePatterns/DuckCall.java
+++ app/src/main/java/com/example/designpatterns/compositePatterns/DuckCall.java
@@ -1,8 +1,26 @@
 package com.example.designpatterns.compositePatterns;
 
+import com.example.designpatterns.compositePatterns.observer.Observable;
+import com.example.designpatterns.compositePatterns.observer.Observer;
+
 public class DuckCall implements Quackable {
+    Observable observable;
+    public DuckCall(){
+        observable = new Observable(this);
+    }
     @Override
     public void quack() {
         System.out.println("Kwak");
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observable.registerObserver(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observable.notifyObservers();
+
     }
 }
