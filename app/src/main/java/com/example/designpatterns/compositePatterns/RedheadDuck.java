1be5a1f0948cb638a69bd1afecf4c1e4f92524e1
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-05 13:38:07 +0300

    refactor RedheadDuck

--- app/src/main/java/com/example/designpatterns/compositePatterns/RedheadDuck.java
+++ app/src/main/java/com/example/designpatterns/compositePatterns/RedheadDuck.java
@@ -1,8 +1,27 @@
 package com.example.designpatterns.compositePatterns;
 
+import com.example.designpatterns.compositePatterns.observer.Observable;
+import com.example.designpatterns.compositePatterns.observer.Observer;
+
 public class RedheadDuck implements Quackable {
+    Observable observable;
+    public RedheadDuck(){
+        observable = new Observable(this);
+    }
     @Override
     public void quack() {
         System.out.println("Quack");
+        notifyObservers();
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observable.registerObserver(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observable.notifyObservers();
+
     }
 }
