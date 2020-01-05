da5f0456a0c249f825b29ab109b4a597c3c9cc34
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-05 12:31:46 +0300

    refactor MallardDuckC

--- app/src/main/java/com/example/designpatterns/compositePatterns/MallardDuckC.java
+++ app/src/main/java/com/example/designpatterns/compositePatterns/MallardDuckC.java
@@ -1,8 +1,27 @@
 package com.example.designpatterns.compositePatterns;
 
+import com.example.designpatterns.compositePatterns.observer.Observable;
+
+import java.util.Observer;
+
 public class MallardDuckC implements Quackable {
+    Observable observable;
+    public MallardDuckC(){
+        observable = new Observable(this);
+    }
     @Override
     public void quack() {
         System.out.println("Quack");
+        notifyObservers();
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observable.registerObserver(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observable.notifyObservers();
     }
 }
