89096fc7c1487776dc8f62cb8eb22f6a3fd18387
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-06 13:27:54 +0300

    Example change -> (intelij)

--- app/src/main/java/com/example/designpatterns/compositePatterns/mvc/BeatModelInterface.java
+++ app/src/main/java/com/example/designpatterns/compositePatterns/mvc/BeatModelInterface.java
@@ -1,7 +1,7 @@
 package com.example.designpatterns.compositePatterns.mvc;
 
 public interface BeatModelInterface {
-    void initialize();
+    /*void initialize();
     void on();
     void off();
     void setBPM(int bpm);
@@ -9,5 +9,6 @@ public interface BeatModelInterface {
     void registerObserver(BeatObserver o);
     void removeObserver(BeatObserver o);
     void registerObserver(BPMObserver o);
-    void removeObserver(BPMObserver o);
+    void removeObserver(BPMObserver o);*/
+    //create new repository for MVC
 }
